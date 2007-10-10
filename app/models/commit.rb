d03aba1ec283fae80d731f70312af1d540b14714
Todd Lipcon <todd@janus.corp.amiestreet.com>
2007-10-10 17:13:13 -0400

    Refactor and clean up diff model a little

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -2,16 +2,24 @@ class Commit < ActiveRecord::Base
   validates_format_of :sha1, :with => /^[a-z0-9]+$/
   validates_length_of :sha1, :is => 40
 
+  def check_valid
+    if ! self.valid?
+      throw Exception.new("Cannot operate on invalid commit")
+    end
+  end
+
   def repository_dir
     "/home/todd/arb/arb"
   end
 
-  def diff_tree
-    if ! self.valid?
-      throw Exception.new("Cannot diff invalid commit")
-    end
+  def in_repo
+    Dir.chdir(self.repository_dir) { yield }
+  end
 
-    Dir.chdir(self.repository_dir) do
+  def diff_tree
+    check_valid
+
+    in_repo do
       return `git-diff-tree --no-commit-id -C --cc #{self.sha1}`
     end
   end
