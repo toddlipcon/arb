ef0b356bb4599779f06c4159886b1e39ca904cd7
Todd Lipcon <todd@janus.corp.amiestreet.com>
2007-10-12 19:20:58 -0400

    Modified activerecord_json to include id columns

--- lib/activerecord_json.rb
+++ lib/activerecord_json.rb
@@ -1,12 +1,12 @@
 require 'json/objects'
 
 class ActiveRecord::Base
-  # Converts the ActiveRecord object to a JSON string. Only columns with simple
-  # types are included in this object so no recursion problems can occur.
+  include Reloadable
+
   def to_json
     result = Hash.new
 
-    self.class.content_columns.each do |column|
+    self.class.columns.each do |column|
       result[column.name.to_sym] = self.send(column.name)
     end
 
