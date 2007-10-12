ee2627289a440fdb849226a1969003aa91fc0ebb
Todd Lipcon <todd@janus.corp.amiestreet.com>
2007-10-12 18:55:34 -0400

    Add functions for HTTP get/post to ARB.pm. Add arb-commit-info test script

--- perllib/ARB.pm
+++ perllib/ARB.pm
@@ -3,7 +3,14 @@ package ARB;
 use warnings;
 use strict;
 
+use JSON;
+use LWP::UserAgent;
+
 my $PROJECT_REGEX = qr#^/files/git/repos/[^/]+/(.+?)/?$#;
+
+my $WEBAPP_BASEURL = 'http://janus:3000/';
+
+my $USER_AGENT_TIMEOUT = 5;
 
 ############################################################
 
@@ -43,6 +50,91 @@ sub deduce_project {
     return undef;
 }
 
+
+=item get_current_developer
+
+Returns the username of the current developer
+
+=cut
+
+sub get_current_developer {
+    chomp(my $me = `whoami`);
+    return $me;
+}
+
+
+=item create_review
+
+Returns the unique id for a new review in the database
+
+=cut
+
+sub create_review {
+    my $response = post_json_request('/review/new/json',
+                                     {
+                                         repository => get_current_repository(),
+                                         developer => get_current_developer()
+                                        });
+    die "nyi";
+}
+
+=item get_review($id)
+
+Returns an ARB::Review instance for the given review id, or C<undef> if it
+is not found.
+
+=cut
+
+sub get_review {
+    my $id = (@_);
+
+    return get_json_request('/review/' . $id . '/json');
+#TODO(nyi) ARB::Review object
+}
+
+=item get_json_request($path, \%params)
+
+=item post_json_request($path, \%params)
+
+Does an HTTP POST to the review server with the given path and parameters.
+Expects that the response is JSON encoded and decodes it to a perl hashref.
+
+=cut
+
+sub post_json_request {
+    return json_request('post', @_);
+}
+
+sub get_json_request {
+    return json_request('get', @_);
+}
+
+sub json_request {
+    my ($method, $path, $params) = @_;
+
+    die "bad method" unless $method =~ /^get|post$/i;
+
+    my $url = $WEBAPP_BASEURL . $path;
+
+    my $response = _create_useragent()->$method($url, $params);
+    return _decode_response($url, uc $method, $response);
+}
+
+sub _create_useragent {
+    my $ua = LWP::UserAgent->new;
+    $ua->timeout($USER_AGENT_TIMEOUT);
+
+    return $ua;
+}
+
+sub _decode_response {
+    my ($url, $method, $response) = @_;
+    die "unsuccessful $method to $url: " . $response->status_line
+        unless $response->is_success;
+
+    my $json = new JSON(unmapping => 1);
+    return $json->jsonToObj($response->content);
+}
 
 =back
 
