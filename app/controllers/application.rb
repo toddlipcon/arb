96bcf2ea180125586590b06b10b16cb19e21ae4a
Todd Lipcon <todd@turbo.corp.amiestreet.com>
2007-10-17 17:00:41 -0400

    Add HTTP basic auth with fake backend that authenticates user 'todd'

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,6 +2,8 @@
 # Likewise, all the methods added will be available for all controllers.
 class ApplicationController < ActionController::Base
 
+  before_filter :authenticate
+
   def render(args = {})
     if ! args[:json].nil?
       @headers['Content-Type'] = 'text/javascript'
@@ -14,4 +16,47 @@ class ApplicationController < ActionController::Base
     end
   end
 
+
+##
+# Taken from http://wiki.rubyonrails.org/rails/pages/HowtoAuthenticateWithHTTP
+##
+  def get_auth_data
+    auth_data = nil
+    [
+      'REDIRECT_REDIRECT_X_HTTP_AUTHORIZATION',
+      'REDIRECT_X_HTTP_AUTHORIZATION',
+      'X-HTTP_AUTHORIZATION', 
+      'HTTP_AUTHORIZATION'
+    ].each do |key|
+      if request.env.has_key?(key)
+        auth_data = request.env[key].to_s.split
+        break
+      end
+    end
+
+    if auth_data && auth_data[0] == 'Basic' 
+      return Base64.decode64(auth_data[1]).split(':')[0..1] 
+    end 
+  end
+
+
+##
+# Authenticate user using HTTP Basic Auth
+##
+  def authenticate
+    login, password = get_auth_data
+    if authorize(login, password)
+      session[:username] = login
+      return true
+    end
+
+    response.headers["Status"] = 'Unauthorized'
+    response.headers['WWW-Authenticate'] = 'Basic realm="ARB"'
+    render :text => "Authentication required", :status => 401
+  end
+
+
+  def authorize(username, password)
+    return username == 'todd'
+  end
 end
