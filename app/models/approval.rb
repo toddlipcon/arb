4d212a92b133016b3dde3b72708a2ec136eba0da
Todd Lipcon <todd@janus.corp.amiestreet.com>
2007-10-24 19:27:16 -0400

    Fix approval model to reference commit_sha1 instead of commit in DB

--- app/models/approval.rb
+++ app/models/approval.rb
@@ -1,7 +1,6 @@
 class Approval < ActiveRecord::Base
-
-  belongs_to :commit
-
   validates_presence_of :approved_by
   validates_presence_of :approved_on
+
+  validates_length_of :commit_sha1, :is => 40
 end
