099a1f344a9069aac448a8641c3ea6cba8f2cbcd
Todd Lipcon <todd@janus.corp.amiestreet.com>
2007-11-01 13:54:42 -0400

    Change review model to diff against the stored "against" sha1

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,8 @@ class Review < ActiveRecord::Base
   validates_presence_of :project
   belongs_to :project
 
+  validates_length_of :against_sha1, :is => 40
+
   def branch
     branch = self.project.review_repository.branch('review-' + self.id.to_s)
     if branch.nil? || !branch.exists_in_repository?
@@ -16,7 +18,7 @@ class Review < ActiveRecord::Base
     branch = self.branch
 
     project.review_repository.git_fetch_from('main')
-    branch.rev_list_from('remotes/main/master').map do |sha1|
+    branch.rev_list_from(self.against_sha1).map do |sha1|
       ArbCommit.new(project, sha1)
     end
   end
